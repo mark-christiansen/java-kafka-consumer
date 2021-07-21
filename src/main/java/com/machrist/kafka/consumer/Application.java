b3febac26a4a5d2efdd9f29b8eed7a979f0059ef
Mark Christiansen <mark.christiansen@wnins.com>
2021-07-21 15:49:46 -0500

    modified for WN

--- src/main/java/com/machrist/kafka/consumer/Application.java
+++ src/main/java/com/machrist/kafka/consumer/Application.java
@@ -25,13 +25,15 @@ import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 
+import static java.lang.String.format;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     private static final String ID_FIELD = "id";
     private static final String OPERATION_FIELD = "gwcbi___operation";
-    private static final String TIMESTAMP_FIELD = "gwcbi___payload_ts_ms";
+    private static final String TIMESTAMP_FIELD = "gwcbi___connector_ts_ms";
 
     @Autowired
     private KafkaConsumer<GenericRecord, GenericRecord> consumer;
@@ -49,10 +51,16 @@ public class Application implements CommandLineRunner {
 
         String topicName = cmd.getOptionValue("topic");
         boolean logValues = cmd.getOptionValue("log") != null && Boolean.parseBoolean(cmd.getOptionValue("log"));
+        boolean cda = cmd.getOptionValue("cda") != null && Boolean.parseBoolean(cmd.getOptionValue("cda"));
 
         Set<String> tableNames = new HashSet<>();
         if (cmd.getOptionValues("tables") != null) {
             tableNames.addAll(Arrays.asList(cmd.getOptionValues("tables")));
+        }
+
+        Set<String> sources = new HashSet<>();
+        if (cmd.getOptionValues("sources") != null) {
+            sources.addAll(Arrays.asList(cmd.getOptionValues("sources")));
         }
 
         log.info("subscribing to topic {}", topicName);
@@ -69,49 +77,79 @@ public class Application implements CommandLineRunner {
                     GenericRecord key = r.key();
                     Schema keySchema = key.getSchema();
 
-                    String tableName = keySchema.getName().substring(0, keySchema.getName().length() - 4);
+                    String keySchemaName = keySchema.getFullName();
+                    String schemaName;
+                    // expect something like "uat.raw.cda.bctl_condcmsnsubplantier_ext_key" for cda
+                    if (cda && keySchemaName.endsWith("_key")) {
+                        schemaName = keySchemaName.substring(0, keySchemaName.length() - 4);
+                    // expect something like "com.attunity.queue.msg.chips-qa.TSTDTA.CINMAD.KeyRecord" for non-cda
+                    } else if (!cda && keySchemaName.endsWith(".KeyRecord")) {
+                        schemaName = keySchemaName.substring(0, keySchemaName.length() - 10);
+                    } else {
+                        //log.debug(format("Unexpected key schema name %s (cda=%b)", keySchemaName, cda));
+                        schemaName = "default";
+                       // throw new RuntimeException(format("Unexpected key schema name %s (cda=%b)", keySchemaName, cda));
+                    }
+
+                    String tableName = schemaName.substring(schemaName.lastIndexOf(".") + 1);
                     if (tableNames.isEmpty() || tableNames.contains(tableName)) {
 
                         TableMetrics metrics = tableMetrics.computeIfAbsent(tableName, (k) -> new TableMetrics(tableName));
                         metrics.addMessage();
 
                         Map<String, Object> keyValues = getRecordValues(keySchema, key);
-                        metrics.addId((Long) keyValues.get(ID_FIELD));
-                        if (logValues) {
-                            log.info("{}", keyValues);
+                        if (cda) {
+                            metrics.addId((Long) keyValues.get(ID_FIELD));
                         }
 
                         GenericRecord value = r.value();
                         if (value != null) {
 
-                            Map<String, Object> valueValues = getRecordValues(value.getSchema(), value);
-                            Object operation = valueValues.get(OPERATION_FIELD);
-                            if (operation != null) {
-                                switch ((Integer) operation) {
-                                    case 0:
-                                        metrics.addInitialLoad();
-                                        break;
-                                    case 1:
-                                        metrics.addDelete();
-                                        break;
-                                    case 2:
-                                        metrics.addInsert();
-                                        break;
-                                    case 4:
-                                        metrics.addUpdate();
-                                        break;
-                                }
+                            //String sourceSystem = ((GenericRecord)value.get("account")).get("sourceSystem").toString();
+                            String sourceSystem = value.get("sourceSystem") != null ? value.get("sourceSystem").toString() : null;
+                            if (!sources.isEmpty() && !sources.contains(sourceSystem)) {
+                                return;
                             }
 
+                            Map<String, Object> valueValues = getRecordValues(value.getSchema(), value);
                             if (logValues) {
                                 log.info("{}", valueValues);
                             }
+
+                            if (cda) {
+
+                                Object operation = valueValues.get(OPERATION_FIELD);
+                                if (operation != null) {
+                                    switch ((Integer) operation) {
+                                        case 0:
+                                            metrics.addInitialLoad();
+                                            break;
+                                        case 1:
+                                            metrics.addDelete();
+                                            break;
+                                        case 2:
+                                            metrics.addInsert();
+                                            break;
+                                        case 4:
+                                            metrics.addUpdate();
+                                            break;
+                                    }
+                                }
+
+                            } else {
+                                GenericRecord headers = (GenericRecord) value.get("headers");
+                                if (headers != null) {
+                                    log.info("Table Record [Operation: {}, TransactionId: {}, Timestamp: {}, Change Seq: {}, Event #: {}, Stream Pos: {}]",
+                                            headers.get("operation"), headers.get("transactionId"), headers.get("timestamp"), headers.get("changeSequence"),
+                                            headers.get("transactionEventCounter"), headers.get("streamPosition"));
+                                }
+                            }
+
                         } else {
                             if (logValues) {
-                                log.info("null");
+                                log.info("Tombstone for key {}", keyValues);
                             }
                         }
-
                     }
                 });
                 totalCount += records.count();
@@ -134,6 +172,8 @@ public class Application implements CommandLineRunner {
         options.addOption(Option.builder("t").longOpt("topic").optionalArg(false).hasArg(true).desc("Topic to consume").type(String.class).build());
         options.addOption(Option.builder("l").longOpt("log").optionalArg(true).hasArg(true).desc("Log message values to console").type(Boolean.class).build());
         options.addOption(Option.builder("i").longOpt("tables").optionalArg(true).hasArgs().desc("Included tables").type(String.class).build());
+        options.addOption(Option.builder("s").longOpt("sources").optionalArg(true).hasArgs().desc("Included sources").type(String.class).build());
+        options.addOption(Option.builder("c").longOpt("cda").optionalArg(true).hasArg(true).desc("Consuming CDA topic").type(Boolean.class).build());
         return options;
     }
 
@@ -201,7 +241,11 @@ public class Application implements CommandLineRunner {
             }
             switch (fieldSchema.getName()) {
                 case "string":
-                    return new String(((Utf8) value).getBytes());
+                    if (value instanceof Utf8) {
+                        return new String(((Utf8) value).getBytes());
+                    } else {
+                        return value;
+                    }
             }
         }
         return value;
